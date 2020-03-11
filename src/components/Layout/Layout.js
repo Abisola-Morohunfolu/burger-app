7a9642273599ac773d17b18f763b6d1fef09ef92
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2020-03-11 21:39:37 +0100

    added-sidedrawer-for-mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = props => (
 	<Auxiliary>
-		<div>ToolBar, Sidebar, Backdrop</div>
+		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>{props.children}</main>
 	</Auxiliary>
 );
